9b4dd4829f5d4a16a4446421b12f8c0af889fd5d
ashraf <dolaposoftech@gmail.com>
2026-05-06 09:06:24 +0100

    commit all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import {
   About,
   Cocktail,
@@ -13,6 +17,13 @@ import { loader as landingLoader } from "./pages/Landing.jsx";
 import { loader as SingleCocktailLoader } from "./pages/Cocktail.jsx";
 import { action as newsletterAction } from "./pages/Newsletter.jsx";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,7 +34,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Landing />,
         errorElement: <SinglePageError />,
-        loader: landingLoader,
+        loader: landingLoader(queryClient),
       },
       { path: "about", element: <About /> },
       {
@@ -37,6 +48,11 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 export default App;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,55 +1,39 @@
-// import { useLoaderData } from "react-router-dom";
-// import axios from "axios";
-// import CocktailList from "../componenets/CocktailList";
-// import SearchTerm from "../componenets/SearchTerm";
-
-// const cocktailSearchUrl = searchTerm
-//   ? `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
-//   : `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`;
-// // "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
-
-// export const loader = async ({ request }) => {
-//   const url = new URL(request.url);
-
-//   const searchTerm = url.searchParams.get("search") || "";
-//   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-
-//   // console.log(response.data.drinks[0]);  // this is the road to the data we want
-//   return { drinks: response.data.drinks, searchTerm };
-// };
-
-// const Landing = () => {
-//   const { drinks, searchTerm } = useLoaderData();
-
-//   return (
-//     <>
-//       <SearchTerm searchTerm={searchTerm} />
-//       <CocktailList drinks={drinks} />
-//     </>
-//   );
-// };
-// export default Landing;
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
 import CocktailList from "../componenets/CocktailList";
 import SearchTerm from "../componenets/SearchTerm";
+import { useQuery } from "@tanstack/react-query";
 
-export const loader = async ({ request }) => {
-  const url = new URL(request.url);
-  const searchTerm = url.searchParams.get("search") || "";
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || "all"],
+    queryFn: async () => {
+      const apiUrl = searchTerm
+        ? `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        : `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`;
 
-  const apiUrl = searchTerm
-    ? `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
-    : `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`;
-
-  const response = await axios.get(apiUrl);
-
-  return { drinks: response.data.drinks || [], searchTerm };
+      const response = await axios.get(apiUrl);
+      return response.data.drinks || [];
+    },
+  };
 };
 
-const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
+export const loader =
+  (QueryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get("search") || "";
+    await QueryClient.ensureQueryData(searchCocktailsQuery(searchTerm));
+    return { searchTerm };
+  };
 
+const Landing = () => {
+  const { searchTerm } = useLoaderData();
+  const { data: drinks, isLoading } = useQuery(
+    searchCocktailsQuery(searchTerm),
+  );
+
+  if (isLoading) return <h4>Loading...</h4>;
   return (
     <>
       <SearchTerm searchTerm={searchTerm} />
